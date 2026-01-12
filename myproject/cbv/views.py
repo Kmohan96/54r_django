ac316fd7f2d5e0e8504e9819020d29deb52c329a
Kmohan96 <mohansaitejak@gmail.com>
2026-01-12 21:16:03 +0530

    cbv updated

--- myproject/cbv/views.py
+++ myproject/cbv/views.py
@@ -18,17 +18,60 @@ class DemoClass(View):
     def delete(self,request):
         return JsonResponse({"info":"delete"})
     
+
 @method_decorator(csrf_exempt,name='dispatch')
 class PaymentInfo(View):
     def post(self,request):
         try:
             data=json.loads(request.body)
-            payment= PaymentDetails.objects.create(payment_status=data["status"],
-            amount=data["amount"],
-            payment_mode=data["mode"],
-            user_email=data["email"],
-            order_id=data["order_id"]                                      
+            payment= PaymentDetails.objects.create(           
+                payment_status=data["status"],
+                amount=data["amount"],
+                payment_mode=data["mode"],
+                user_email=data["email"],
+                order_id=data["order_id"]                                      
             )
-            return JsonResponse({'message':'posted successfully',"transactionid":str(payment.transaction_id)},status=201)
+            return JsonResponse({'message':'posted successfully',"transactionid":str(payment.transaction_id)},status=201,safe=False)
         except Exception as e:
-            return JsonResponse({'message:"error occured'},status=500)
+            return JsonResponse({"msg":"error"},status=400)
+          
+
+
+        
+  
+  
+#   "order_id": "ord124",
+#   "email": "mohan@gmail.com",
+#   "amount": "60000.00",
+#   "status": "ok",
+#   "mode":"online"
+  
+
+    def get(self,request):
+        try:
+            mydatabase=list(PaymentDetails.objects.values())
+            return JsonResponse({"data":mydatabase,"msg":"successfully fetched"},status=200)
+        except Exception as e:
+            return JsonResponse({"msg":"error"},status=400)
+        
+    def put(self,request):
+        try:
+            data=json.loads(request.body)
+            order_id=data.get("order_id")
+            new_email=data.get("email")
+            existing_data=PaymentDetails.objects.get(order_id=order_id)
+            existing_data.user_email=new_email
+            existing_data.save()
+            return JsonResponse({"status":"data updated successfully"},status=200)
+        except Exception as e:
+            return JsonResponse({"msg":"error"},status=400)
+    def delete(self,request):
+        try:
+            data=json.loads(request.body)
+            order_id=data.get("order_id")
+            existing_data=PaymentDetails.objects.get(order_id=order_id)
+            existing_data.delete()
+            return JsonResponse({"status":"data deleted successfully"},status=200)
+        except Exception as e:
+            return JsonResponse({"msg":"error"},status=400)
+    
